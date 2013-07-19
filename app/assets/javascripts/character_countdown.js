e52d281e8afc6e15840a21d3ae2db3a19d77b2bf
Audrius Jurgelionis <ajurge@gmail.com>
2013-07-19 16:15:21 +0300

    Fix character countdown javascript

--- app/assets/javascripts/character_countdown.js
+++ app/assets/javascripts/character_countdown.js
@@ -1,20 +1,27 @@
 function updateCountdown() {
   // 140 characters max
-  var left = 140 - jQuery('.micropost_text_area').val().length;
+  var left = 140 - jQuery('#micropost_content').val().length;
+  jQuery(".countdown").css('color', 'gray');
+  if(left == 0) {
+    jQuery(".countdown").css('color', 'black');
+  }
   if(left == 1) {
     var charactersLeft = ' character left.'
+    jQuery(".countdown").css('color', 'black');
   }
   else if(left < 0){
     var charactersLeft = ' characters too many.'
+    jQuery(".countdown").css('color', 'red');
   }
   else{
     var charactersLeft = ' characters left.'
   }
   jQuery('.countdown').text(Math.abs(left) + charactersLeft);
+
 }
 
 jQuery(document).ready(function($) {
   updateCountdown();
-  $('.micropost_text_area').change(updateCountdown);
-  $('.micropost_text_area').keyup(updateCountdown);
+  $('#micropost_content').change(updateCountdown);
+  $('#micropost_content').keyup(updateCountdown);
 });
